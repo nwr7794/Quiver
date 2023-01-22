d592e14c3aa7f74c5b89d6e25ca7d100c756f5a6
John Tokarowski <johntokarowski@Tokarowski-MBP-Personal.local>
2023-01-22 06:10:14 -0700

    additional handlers

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-import json
 from flask import Flask, Markup, request, redirect, render_template, jsonify
+from flask_cors import CORS
 import requests
 from datetime import date
 import time
 import os
-from flask_cors import CORS
+import json
+from fred import Fred
+import pandas as pd
+
+#INIT THE CLIENT
+API_KEY = os.environ.get('FRED_API_KEY')
+fr = Fred(api_key=API_KEY,response_type='json')
 
 #creates instance of app
 app = Flask(__name__)
@@ -12,9 +18,18 @@ CORS(app)
 app.config.from_object(__name__)
 
 @app.route("/")
-def pingroute():
-
+def health():
     return 'OK'    
+
+@app.route("/retrievedata")
+def retrieveData():
+
+    return(fr.category.children(97))
+
+@app.route("/fredsearch")
+def fredsearch():
+
+    return 'Search results'    
 
 #instantiate app
 if __name__ == "__main__":
