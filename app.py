764e9b55b4ebf92283195abf2c95ad1b7c47c54a
John Tokarowski <johntokarowski@Tokarowski-MBP-Personal.local>
2023-02-15 23:58:31 +0800

    added log statements

--- app.py
+++ app.py
@@ -30,11 +30,14 @@ def fredsearch():
     if searchKey == None:
         return "Error: no search terms provided"
 
+    print("recieved search term: " + str(searchKey))
+
     params = {'limit':50,}
     res = fr.series.search(searchKey,params=params)
     observations = json.dumps(json.loads(res))
     SEARCH_RES_DF = pd.read_json(observations)
     SEARCH_RES_DF_NEW = SEARCH_RES_DF['seriess']
+    print("successfully fetched the search results. returning repsonse now")
     # Convert back to JSON
     return(SEARCH_RES_DF_NEW.to_json())
 
@@ -49,6 +52,14 @@ def data_request_hof():
     #expect data_series to be a list of objects
     requested_series_identifier_list  = request.json['requested_series_identifier_list']
 
+    if target_output_frequency == None:
+        return "Error: must specify a target frequency", 404    
+    if requested_series_identifier_list == None:
+        return "Error: Must provide at least 1 requested data series identifier", 404
+    
+    print('requested output frequency', target_output_frequency)
+    print('requested series', requested_series_identifier_list)
+
     #define list of objects to send to the next step
     outgoing_dataseries_list = []
     outgoing_df_list = []
@@ -56,11 +67,14 @@ def data_request_hof():
     latest_date = None
 
     #looooooooooooop
+    i = 0
     for requested_series in requested_series_identifier_list:
 
         #2 params on each series object
         series_identifier = requested_series['series_identifier']
         fill_methodology = requested_series['fill_methodology']
+
+        print("iteration number: ", i, " with identifier: ", series_identifier," and fill methodology: ", fill_methodology)
 
         #define outgoing data  #TODO: future state hold on to the name and description from the original search result
         detailed_series_data = {
@@ -75,16 +89,22 @@ def data_request_hof():
             }
 
         #get the raw data from FRED
+        print("now querying FRED for raw data")
         detailed_series_data['series_raw_api_response'] = retrieve_raw_fred_data(series_identifier)
+        detailed_series_data['series_raw_observations'] = detailed_series_data['series_raw_api_response']['observations']
+        print("now querying FRED for descriptive info")
         detailed_series_data['series_name'] = retrieve_series_name(series_identifier)
         detailed_series_data['series_frequency'] = retrieve_series_frequency(series_identifier)
-        detailed_series_data['series_raw_observations'] = detailed_series_data['series_raw_api_response']['observations']
+        
 
         # convert to a dataframe and clean it up
+        print("shifting raw data into dataframe")
         intermediate_state_dataframe = object_2_dataframe(detailed_series_data['series_name'], detailed_series_data['series_raw_observations'])
+        print("cleaning dataframe")
         detailed_series_data['series_dataframed'] = df_cleanup(intermediate_state_dataframe, ['realtime_start','realtime_end'])
         
         #calc min and max date here (looking across all the dataframes)
+        print("checking local max/min vs. global")
         local_min  = detailed_series_data['series_dataframed'].index.min()
         local_max  = detailed_series_data['series_dataframed'].index.max()
         
@@ -100,24 +120,29 @@ def data_request_hof():
         elif local_max > latest_date:
             latest_date = local_max
 
-        #add to list of objects    
+        #add to list of objects
+        print("adding resulting df to main object")
         outgoing_dataseries_list.append(detailed_series_data)
         outgoing_df_list.append(detailed_series_data['series_dataframed'])
-
-    #detailed_series_data['series_dataframed'] = detailed_series_data['series_dataframed'].resample(target_output_frequency).ffill()
         
     #create the overarching earliest to latest timerange
+    print("setting up main dataframe")
     main_frame = pd.DataFrame(np.nan, index=pd.date_range(earliest_date, latest_date), columns=['NaNs'])
     main_frame.drop(axis=1, columns=['NaNs'], inplace=True)
 
     # shift every series over to the new frame (expect lots of NaNs)
+    print("beginning loop to join dataframes into main")
     for dataseries_object in outgoing_dataseries_list:
         #resample & join the frame into main
         #TODO: figure out how to fill the NaNs that are created when the date range extends before or after the dataset
+        print("now backfilling series id: ",dataseries_object['series_identifier']," and merging into the main dataframe")
         main_frame = main_frame.join(resample_hof(dataseries_object, target_output_frequency))
+        print("successfully backfilled + merged series id: ",dataseries_object['series_identifier'])
 
     # return the resulting main dataframe as json to the UI
     main_frame.index = main_frame.index.strftime('%Y/%m/%d') 
+
+    print("main dataframe completed. returning the result")
     
     return main_frame.to_json()
     
